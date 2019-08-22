391c279269b6cc64662e4ff698229a8247458b7c
Arledis <arleidysamaya_18@hotmail.com>
2019-08-22 15:26:41 +0100

    Adding calculator and updated

---
+++ pda_testing/js_calculator_start_point/tests/integration/tests.js
@@ -0,0 +1,120 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+describe('calculator functionality', function() {
+  beforeEach(function() {
+    browser.ignoreSynchronization = true;
+    browser.get('http://localhost:3000');
+  });
+
+  // write integration tests here in the form of "it should do something..."
+// 1. Do the number buttons update the display of the running total?
+  it('should have working number buttons', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  });
+
+
+// 2. Do the arithmetical operations update the display with the result of the operation?
+  it('should update the display with the result of the operation', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number8')).click();
+    element(by.css('#number3')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('83');
+  });
+
+  it('should be able to add numbers and display the result of the operation', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('12');
+  });
+
+  it('should be able to subtract numbers and display the result of the operation', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number8')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4');
+  });
+
+  it('should be able to multiply numbers and display the result of the operation', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('10');
+  });
+
+  it('should be able to divide numbers and display the result of the operation', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('3');
+  });
+
+  // 3. Can multiple operations be chained together?
+
+  it('should multiple operations chained together', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number3')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number6')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('15');
+  });
+
+  // 4. Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
+
+  it('should work as expected for a negative number result', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-8');
+  });
+
+  it('should work as expected for decimals numbers result', function(){
+  element(by.css('#number8')).click();
+  element(by.css('#operator_divide')).click();
+  element(by.css('#number3')).click();
+  element(by.css('#operator_equals')).click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('2.6666666666666665');
+});
+
+it('should work as expected for large numbers result', function(){
+  element(by.css('#number4')).click();
+  element(by.css('#number3')).click();
+  element(by.css('#number7')).click();
+  element(by.css('#number3')).click();
+  element(by.css('#number5')).click();
+  element(by.css('#number6')).click();
+  element(by.css('#operator_multiply')).click();
+  element(by.css('#number9')).click();
+  element(by.css('#operator_equals')).click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('3936204');
+});
+
+// 5. What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? Write a test to describe what you'd prefer to happen, and then correct the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
+it('should display Infinity on screen if you divide by zero', function(){
+   running_total = element(by.css('#running_total'))
+   element(by.css('#number1')).click();
+   element(by.css('#operator_divide')).click();
+   element(by.css('#number0')).click();
+   element(by.css('#operator_equals')).click();
+   expect(running_total.getAttribute('value')).to.eventually.equal('Infinity');
+ });
+
+});
